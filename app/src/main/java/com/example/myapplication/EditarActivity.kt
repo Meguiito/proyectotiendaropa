566cdba199b1a61c76844a5ddf13595ebbde1054
benjaminespinoza <benjamintwo2002@gmail.com>
2024-05-10 22:46:15 -0400

    Seccion Editar

---
+++ app/src/main/java/com/example/myapplication/EditarActivity.kt
@@ -0,0 +1,124 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.unit.sp
+
+import androidx.compose.material3.Button
+import androidx.compose.ui.text.style.TextAlign
+
+class EditarActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EditarScreen()
+                }
+            }
+        }
+    }
+
+    data class Product(
+        val type: String,
+        val size: String,
+        val model: String,
+        val price: Double
+    )
+
+    @Composable
+    fun EditarScreen() {
+        val products = listOf(
+            Product("pantalon", "L", "Nike", 20.000),
+            // Agrega más productos si es necesario
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ProductTable(products = products)
+        }
+    }
+
+    @Composable
+    fun ProductTable(products: List<Product>) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            item {
+                Text(
+                    "Editar Producto",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 45.sp,
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+            }
+            items(products.size) { index ->
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        "Tipo: ${products[index].type}",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(120.dp))
+                    Text(
+                        "Talla: ${products[index].size}",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(120.dp))
+                    Text(
+                        "Modelo: ${products[index].model}",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(120.dp))
+                    Text(
+                        "Precio: ${products[index].price}",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Button(
+                        onClick = {
+                            // Navegación a la pantalla de Consulta
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Editar")
+                    }
+                }
+            }
+        }
+    }
+}
