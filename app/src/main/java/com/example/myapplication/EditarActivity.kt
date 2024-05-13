9934b1c80691130df48b4f05ff75dec7ffbd6783
pabloantivil <pabloantivil16@gmail.com>
2024-05-13 00:32:30 -0400

    Añadidos Titulo y Back Agregado titulo BananaShop y boton para regresar en la pantalla de Editar

--- app/src/main/java/com/example/myapplication/EditarActivity.kt
+++ app/src/main/java/com/example/myapplication/EditarActivity.kt
@@ -1,16 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -39,30 +47,66 @@ class EditarActivity : ComponentActivity() {
 
     @Composable
     fun EditarScreen() {
+        val context = LocalContext.current
         val products = listOf(
             Product("pantalon", "L", "Nike", 20.000),
-            // Agrega más productos si es necesario
         )
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.height(16.dp))
+        BoxWithBackground {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+            ) {
+                // Botón de "volver"
+                IconButton(
+                    onClick = {
+                        val intent = Intent(context, InicioActivity::class.java)
+                        context.startActivity(intent)
+                    },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .background(Color(0xFF00668b), shape = RoundedCornerShape(8.dp))
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowBack,
+                        contentDescription = "Volver",
+                        tint = Color.White
+                    )
+                }
 
-            Text(
-                "Editar Producto",
-                style = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp,
-                    color = Color.White
-                ),
-                modifier = Modifier.padding(bottom = 32.dp)
-            )
-
-            products.forEach { product ->
-                ProductRow(product = product)
                 Spacer(modifier = Modifier.height(16.dp))
+
+                // Título "Banana Shop"
+                Text(
+                    text = "Banana Shop",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Título "Editar Producto" centrado
+                Text(
+                    "Editar Producto",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = Color.White
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                products.forEach { product ->
+                    ProductRow(product = product)
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
             }
         }
     }
