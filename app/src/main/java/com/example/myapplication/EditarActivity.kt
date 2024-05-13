7a23b108aa5306189f5154ec8f4db45263dee09a
pabloantivil <pabloantivil16@gmail.com>
2024-05-13 15:22:57 -0400

    Pantalla de editar recuperado Se habia perdido lo que habia avanzado el Benja Espinoza xd

--- app/src/main/java/com/example/myapplication/EditarActivity.kt
+++ app/src/main/java/com/example/myapplication/EditarActivity.kt
@@ -1,124 +1,194 @@
 package com.example.myapplication
-
 import android.content.Intent
 import android.os.Bundle
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
-import androidx.compose.ui.unit.sp
-
-import androidx.compose.material3.Button
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class EditarActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    EditarScreen()
-                }
+                EditarScreen()
             }
         }
     }
 
+
     data class Product(
-        val type: String,
-        val size: String,
-        val model: String,
-        val price: Double
+        val type: String="",
+        val size: String="",
+        val model: String="",
+        val price: String="",
     )
 
     @Composable
     fun EditarScreen() {
-        val products = listOf(
-            Product("pantalon", "L", "Nike", 20.000),
-            // Agrega más productos si es necesario
-        )
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.height(16.dp))
+        val context = LocalContext.current
 
-            ProductTable(products = products)
-        }
-    }
+        var products by remember { mutableStateOf(listOf(Product())) }
 
-    @Composable
-    fun ProductTable(products: List<Product>) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            item {
+        BoxWithBackground {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+            ) {
+                // Botón de "volver"
+                IconButton(
+                    onClick = {
+                        val intent = Intent(context, InicioActivity::class.java)
+                        context.startActivity(intent)
+                    },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .background(Color(0xFF00668b), shape = RoundedCornerShape(8.dp))
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowBack,
+                        contentDescription = "Volver",
+                        tint = Color.White
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Título "Banana Shop"
+                Text(
+                    text = "Banana Shop",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Título "Editar Producto" centrado
                 Text(
                     "Editar Producto",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 45.sp,
-                    modifier = Modifier.padding(bottom = 32.dp)
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        color = Color.White
+
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+
                 )
-            }
-            items(products.size) { index ->
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    Text(
-                        "Tipo: ${products[index].type}",
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
+                Spacer(modifier = Modifier.height(32.dp))
+                products.forEachIndexed { index, product ->
+                    ProductRow(
+                        product = product,
+                        onEditClick = { editedProduct ->
+                            products = products.toMutableList().also { it[index] = editedProduct }
+                        }
                     )
-                    Spacer(modifier = Modifier.height(120.dp))
-                    Text(
-                        "Talla: ${products[index].size}",
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(120.dp))
-                    Text(
-                        "Modelo: ${products[index].model}",
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(120.dp))
-                    Text(
-                        "Precio: ${products[index].price}",
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Button(
-                        onClick = {
-                            // Navegación a la pantalla de Consulta
-                        },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(text = "Editara")
-                    }
                 }
             }
         }
+    }
+
+
+
+
+    @Composable
+    fun ProductRow(product: Product, onEditClick: (Product) -> Unit) {
+        var editedProduct by remember { mutableStateOf(product) }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            TextField(
+                value = editedProduct.type,
+                onValueChange = { editedProduct = editedProduct.copy(type = it) },
+                label = { Text("Tipo") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            TextField(
+                value = editedProduct.size,
+                onValueChange = { editedProduct = editedProduct.copy(size = it) },
+                label = { Text("Talla") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            TextField(
+                value = editedProduct.model,
+                onValueChange = { editedProduct = editedProduct.copy(model = it) },
+                label = { Text("Modelo") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            TextField(
+                value = editedProduct.price,
+                onValueChange = { editedProduct = editedProduct.copy(price = it) },
+                label = { Text("Precio") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Botón de edición
+            Button(
+                onClick = { onEditClick(editedProduct) },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text("Editar")
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+@Composable
+fun BoxWithBackground(
+    content: @Composable () -> Unit,
+    background: Painter = painterResource(id = R.drawable.fotofondo)
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            painter = background,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
+        content()
     }
 }
