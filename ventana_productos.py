64fd8c96aa5da70eb8c32860b20e4fe5b3be3c00
Catalinafonsec4 <cfonseca2022@alu.uct.cl>
2024-06-08 17:53:59 -0400

    Diseño ventana_productos

--- ventana_productos.py
+++ ventana_productos.py
@@ -8,6 +8,7 @@ ventana = tk.Tk()
 ventana.title("Tabla Inventario de Productos")
 ventana.geometry("800x400")
 
+ventana.configure(bg='black')
 logo_path = "logo.png"
 image = Image.open(logo_path)
 image = image.resize((70, 70))
@@ -15,7 +16,8 @@ logo = ImageTk.PhotoImage(image)
 logo_label = tk.Label(ventana, image=logo)
 logo_label.grid(row=0, column=0, padx=20, pady=2, rowspan=2, sticky='nw')
 
-titulo_productos = tk.Label(ventana, text="Productos", font=("Helvetica", 16))
+
+titulo_productos = tk.Label(ventana, text="Productos", font=("Helvetica", 20, "bold"), bg='black', fg='#FFD700')
 titulo_productos.grid(row=0, column=2, columnspan=2, padx=10, pady=10)
 
 # Funcion para abrir la ventana de ventas
@@ -23,12 +25,20 @@ def abrir_ventas():
     ventana.withdraw()  
     tkinapp.ventana_ventas(ventana)
 
-ventas_btn = tk.Button(ventana, text="Tabla ventas", command=abrir_ventas)
+ventas_btn = tk.Button(ventana, text="Tabla ventas", command=abrir_ventas, font=("Helvetica", 14), bg='light grey', fg='yellow', relief='ridge', borderwidth=8)
 ventas_btn.grid(row=0, column=3, padx=10, pady=10, sticky='ne')
 
 # tabla de productos
 columns = ("ID QR", "Tipo", "Talla", "Precio", "Modelo")
 tabla = ttk.Treeview(ventana, columns=columns, show="headings")
+
+# Estilo para la tabla
+style = ttk.Style()
+style.theme_use('clam')
+style.configure("Treeview", font=("Georgia", 12), background="white", foreground="black", fieldbackground="black")
+style.configure("Treeview.Heading", font=("Georgia", 14, "bold"), foreground="yellow")
+style.configure("TButton", font=("Georgia", 12), padding=5, background='black', foreground='yellow')
+style.configure("TLabel", background="black", foreground="yellow")
 
 for col in columns:
     tabla.heading(col, text=col)
@@ -46,20 +56,20 @@ productos = [
 for producto in productos:
     tabla.insert("", tk.END, values=producto)
 
-btn_laterales = tk.Frame(ventana)
+btn_laterales = tk.Frame(ventana,bg='black' )
 btn_laterales.grid(row=1, column=4, rowspan=4, padx=10, pady=10, sticky='n')
 
 # Botones laterales
-vender_btn = tk.Button(btn_laterales, text="Vender Producto")
+vender_btn = tk.Button(btn_laterales, text="Vender Producto",bg='light grey', fg='yellow', relief='ridge', borderwidth=8)
 vender_btn.pack(pady=10)
 
-agregar_btn = tk.Button(btn_laterales, text="Agregar producto")
+agregar_btn = tk.Button(btn_laterales, text="Agregar producto",bg='light grey', fg='yellow', relief='ridge', borderwidth=8)
 agregar_btn.pack(pady=10)
 
-buscar_btn = tk.Button(btn_laterales, text="Buscar Producto")
+buscar_btn = tk.Button(btn_laterales, text="Buscar Producto",bg='light grey', fg='yellow', relief='ridge', borderwidth=8)
 buscar_btn.pack(pady=10)
 
-editar_btn = tk.Button(btn_laterales, text="Editar producto")
+editar_btn = tk.Button(btn_laterales, text="Editar producto",bg='light grey', fg='yellow', relief='ridge', borderwidth=8)
 editar_btn.pack(pady=10)
 
 ventana.grid_rowconfigure(1, weight=1)
